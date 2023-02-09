cbf5228b16e73fd88d737fdfbcda8da04fb61f7f
gvmertens <gvmertens@gmail.com>
2023-02-09 16:33:32 -0300

    20230209 16:34

--- fc3-catalogo-de-videos/domain/src/main/java/br/com/mertens/domain/category/Category.java
+++ fc3-catalogo-de-videos/domain/src/main/java/br/com/mertens/domain/category/Category.java
@@ -5,7 +5,7 @@ import br.com.mertens.domain.validation.ValidationHandler;
 
 import java.time.Instant;
 
-public class Category extends AggregateRoot<CategoryID> {
+public class Category extends AggregateRoot<CategoryID> implements Cloneable {
 
     private String name;
     private String description;
@@ -137,5 +137,14 @@ public class Category extends AggregateRoot<CategoryID> {
 
     public Instant getDeletedAt() {
         return deletedAt;
+    }
+
+    @Override
+    public Category clone() {
+        try {
+            return (Category) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
